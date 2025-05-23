4c7dead2d4469a168c5a176fe56edf90a758bda0
Kashish <kathuriak805@gmail.com>
2025-05-23 17:34:26 +0530

    middleware to protect route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -36,3 +36,27 @@ export const signup = async (req, res) =>{
         res.json({success: false, message: error.message})
     }
 }
+
+// Controller to login a user 
+
+export const login = async(req,res) =>{
+    try {
+        const {email,password} = req.body;
+        const userData = await User.findOne({email});
+
+        const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+
+        if(!isPasswordCorrect){
+            return res.json({ success: false, message: "Invalid credentials"});
+        }
+
+        const token = generateToken(userData._id);
+
+        res.json({success: true, userData, token, message: "Login successfully"})
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
