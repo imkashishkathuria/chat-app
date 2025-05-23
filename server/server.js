2149a26d82d8c57543668ed31c415367bd490680
Kashish <kathuriak805@gmail.com>
2025-05-23 13:07:33 +0530

    database connection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import "dotenv/config"
 import cors from 'cors'
 import http from 'http'
+import { connectDB } from './lib/db.js';
 
 // Create express app and http server
 const app = express();
@@ -13,6 +14,8 @@ app.use(cors())
 
 
 app.use("/api/status", (req, res)=>res.send("Server is live"));
+
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=>console.log("Server is running on PORT: " + PORT));
