216c9b96f829e954f9c4987f4e9e1625c51061d4
Kashish <kathuriak805@gmail.com>
2025-05-24 18:57:23 +0530

    message routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import http from 'http'
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
 
 // Create express app and http server
 const app = express();
@@ -15,7 +16,8 @@ app.use(cors())
 
 
 app.use("/api/status", (req, res)=>res.send("Server is live"));
-app.use("/api/auth", userRouter)
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter)
 
 await connectDB();
 
